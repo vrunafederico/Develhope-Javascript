d04ada9107614cbdf81c693fe3c1cfd481725553
vrunafederico <Federix5@hotmail.com>
2023-02-12 16:16:53 +0100

    Objects_Copying

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,16 @@
-// prova
+const person1 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  };
+  
+const person2 = person1;
+  
+person1.firstName = "Simon";
+
+console.log(person1);
+console.log(person2);
+
+
+//perché in questo caso non vengono assegnati i valori dell'oggetto ma l'indirizzo della cella di memoria.
+//Quindi tutti e due gli oggetti fanno riferimento allo stesso indirizzo di conseguenza modificare un oggetto vuol dire modificare il valore nella cella di memoria
