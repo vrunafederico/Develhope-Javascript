00d8c4adbe2ef753f61b2ddbfa4a2b6449e93bee
vrunafederico <Federix5@hotmail.com>
2023-02-12 18:35:59 +0100

    Array_Methods_Advanced_4

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,61 @@
-// prova
+function uncompleteNotes(notes){
+  Object.keys(notes).forEach(element =>{
+    Object.keys(notes[element].todos).forEach(element2 =>{
+      if(notes[element].todos[element2].done == false){
+        console.log(notes[element].todos[element2].name)
+      }
+    })
+  })
+}
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+uncompleteNotes(notes)
