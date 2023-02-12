e52a2c1521a8169682b0cdc9d423d4e839c84696
vrunafederico <Federix5@hotmail.com>
2023-02-12 16:11:53 +0100

    Conditional_and_Loops_3

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,9 @@
-// prova
+function sumUntil(maxValue) {
+    let sum = 0;
+    for (let i = 1; i <= maxValue; i++) {
+        sum = sum + i;
+      }
+    return sum;
+    }
+    
+console.log(sumUntil(5));
