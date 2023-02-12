92f9f6e7a83149bad835388bea3489f8b6eaf2a4
vrunafederico <Federix5@hotmail.com>
2023-02-12 18:11:05 +0100

    Array_Methods_Advanced_1

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,26 @@
-// prova
+function adultFilter(persons) {
+  let array = new Array();
+  Object.keys(persons).forEach(element => {
+    if(persons[element].age > 17){
+      array.push(persons[element]);
+    }
+  })
+  return array;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const adults = adultFilter(persons);
+console.log(persons);
+console.log(adults);
