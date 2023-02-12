468c5e03e6694f10d2769095563d2cc5133c3aeb
vrunafederico <Federix5@hotmail.com>
2023-02-12 15:24:57 +0100

    Conditional_and_Loops

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,27 @@
-// prova
+function calculateSalary(work){
+    switch (work){
+        case "ceo":
+            return 2200;
+        case "manager":
+            return 1800;
+        case "cto":
+            return 1800;
+        case "developer":
+            return 1500;
+        default: 
+            return 1000;
+    }
+}
+
+
+const ceoSalary = calculateSalary('ceo');
+const managerSalary = calculateSalary('manager');
+const ctoSalary = calculateSalary('cto');
+const developerSalary = calculateSalary('developer');
+const otherSalary = calculateSalary('other');
+
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);
