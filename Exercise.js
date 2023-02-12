9e8ef32724cefe5a4d4765bb5353e9c8b3483270
vrunafederico <Federix5@hotmail.com>
2023-02-12 18:16:52 +0100

    Array_Methods_Advanced_2

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,22 @@
-// prova
+function nicknameMap(persons) {
+  let array = new Array();
+  Object.keys(persons).forEach(element => array.push(persons[element].name + "-" + persons[element].age));
+  return array;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const nicknames = nicknameMap(persons);
+console.log(persons);
+console.log(nicknames);
