dd4ab4c9a16ac8a412a7f32c7f888b22d69180a6
vrunafederico <Federix5@hotmail.com>
2023-02-12 15:59:52 +0100

    Arrow_Funtions

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,8 @@
-// prova
+const sum = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => a / b;
+const log = (value) => console.log(value);
+
+console.log(divide(subtract(multiply(sum(2,4), sum(5,2)), 2), 5));
+
