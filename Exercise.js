974614dd9cc908eebf1b6815d9151d36849cf2fc
vrunafederico <Federix5@hotmail.com>
2023-02-12 15:32:54 +0100

    Conditional_and_Loops_2

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,22 @@
-// prova
+function nicknameMap(persons) {
+    var array = Array();
+    persons.forEach(element => array.push(element.name+"-"+element.age));
+    return array;
+  }
+  
+  const persons = [
+    { name: 'Paul', age: 16 },
+    { name: 'George', age: 17 },
+    { name: 'Lucas', age: 21 },
+    { name: 'Marco', age: 32 },
+    { name: 'Peter', age: 18 },
+    { name: 'Carl', age: 13 },
+    { name: 'Simon', age: 24 },
+    { name: 'Mark', age: 15 },
+    { name: 'Sandra', age: 34 },
+    { name: 'Alice', age: 28 }
+  ];
+
+const nicknames = nicknameMap(persons);
+console.log(persons);
+console.log(nicknames);
