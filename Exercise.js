46befb62a38ee7327ba168d6bf94976c792ca0a9
vrunafederico <Federix5@hotmail.com>
2023-02-12 16:50:16 +0100

    Closures_3

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,11 @@
-// prova
+function multiplyByTwo(val){
+    function inner(){
+        const number = 2;
+        return val*number;
+    }
+
+    return inner();
+
+}
+
+console.log(multiplyByTwo(4));
