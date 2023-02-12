714d4c564d2a9145def984d5e39069b95ecdcf18
vrunafederico <Federix5@hotmail.com>
2023-02-12 18:25:44 +0100

    Array_Methods_Advanced_3

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,24 @@
-// prova
+function calculateAverageAge(persons) {
+  let sum = 0;
+  Object.keys(persons).forEach(element => {
+    sum = sum + persons[element].age; 
+  });
+  return sum / Object.keys(persons).length;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
