03206b1e683db57d1b6de619c889f5cbab7b399b
vrunafederico <federix5@hotmail.com>
2023-02-24 20:23:18 +0100

    Destructoring-1

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,14 @@
-// prova
+//Use the destructuring, instead of a temporary variable, to swap the values of the variables.
+
+let num1 = 10;
+let num2 = 20;
+console.log('Before swap: ', num1, num2); // Before swap: 10 20
+
+// Variable swapping
+// let temp = num2;
+// num2 = num1;
+// num1 = temp;
+
+[num2, num1] = [num1, num2];
+
+console.log('After swap: ', num1, num2); // After swap: 20 10
