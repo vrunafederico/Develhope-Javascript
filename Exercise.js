e4d89ca4be0084d5218c9f5afa9044c7f20f47da
vrunafederico <federix5@hotmail.com>
2023-02-24 20:27:41 +0100

    Destructoring-2

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,17 @@
-// prova
+//Use the destructuring to assign the values just with one code line.
+
+const person = {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  };
+  
+  const [id, firstName, lastName, age] = [person.id, person.firstName, person.lastName, person.age]
+
+  //const id = person.id;
+  //const firstName = person.firstName;
+  //const lastName = person.lastName;
+  //const age = person.age;
+  
+  console.log(id, firstName, lastName, age);
