697fb542691e6e209393bc67c9b121d0427c94e9
vrunafederico <Federix5@hotmail.com>
2023-02-26 15:54:05 +0100

    Rest-3

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,20 @@
-// prova
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+
+const {id, ...rest} = person;
+const personInfo = {...rest};
+
+
+// const id = person.id;
+// const personInfo = {
+//   firstName: person.firstName,
+//   lastName: person.lastName,
+//   age: person.age
+// };
+
+console.log(id, personInfo);
