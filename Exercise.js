88aa363a37ee772c7a361906e6986e2af7eeeaee
vrunafederico <federix5@hotmail.com>
2023-02-26 15:21:40 +0100

    Destructuring-4

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,11 @@
-// prova
+const person = {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  };
+
+
+
+const { id, name, surname, old } = person;
+console.log(id, name, surname, old);
