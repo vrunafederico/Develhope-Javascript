5063619d2b4a4bc5c68c4eb406f9520c2b6051e4
vrunafederico <Federix5@hotmail.com>
2023-02-26 15:31:53 +0100

    Rest-1

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,11 @@
-// prova
+let numberStore = [0, 1, 2];
+let newNumber = 3;
+
+function insert(newNumber, rest){
+  let arr;
+  return arr = [...rest, newNumber];
+}
+
+console.log(insert(newNumber, numberStore));
+numberStore.push(newNumber);
+console.log(numberStore);
