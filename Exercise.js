ce49b2466e2a29645b86a1e42f7a0c88363b2414
vrunafederico <Federix5@hotmail.com>
2023-02-26 16:27:55 +0100

    Rest-4

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,10 @@
-// prova
+function sum(numbers) {
+  let sum = 0;
+  console.log(numbers)
+  numbers.forEach(element => console.log(element));
+  console.log(sum)
+  return sum;
+}
+
+const numbers = [1, 2, 3];
+console.log(sum(numbers));
