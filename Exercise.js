97d994fd1a06127045a9c48c327581c4009aed97
vrunafederico <federix5@hotmail.com>
2023-03-02 20:15:33 +0100

    Templete

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,12 @@
-// prova
+//Perform a code refactoring by using the template literals.
+
+class Person { 
+    constructor(firstName, lastName) { 
+        this.firstName = firstName; 
+        this.lastName = lastName; 
+    } 
+}
+
+
+const developer = new Person("Mario", "Rossi"); 
+console.log(`${developer.firstName} ${developer.lastName}`); 
