9f9b97c965b449f9a38d4754d5b2472be9ddb3f9
vrunafederico <federix5@hotmail.com>
2023-03-02 20:27:32 +0100

    Callback-1

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,14 @@
-// prova
+function printAsyncName(name, print){
+    setTimeout(function () {
+        console.log(name);
+    }, 1000);
+    setTimeout(function () {
+        print()
+    }, 2000);
+}
+
+function print(){
+    console.log("ciao");
+}
+
+printAsyncName("francesco", print);
