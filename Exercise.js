6a67d5d0e756a366387dac15f9a0d71b5fde7c33
vrunafederico <Federix5@hotmail.com>
2023-03-04 13:50:47 +0100

    Callback-3

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,11 @@
-// prova
+const hello = () => {console.log("hello")}
+function repeatHello(callback){
+    let id = setInterval(function (){
+        callback()
+    },1000)
+
+    setTimeout(() => clearInterval(id), 5000);
+}
+
+repeatHello(hello);
+
