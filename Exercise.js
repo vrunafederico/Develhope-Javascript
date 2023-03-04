e433675705bab438ba498f0a3a8050a52e800e28
vrunafederico <Federix5@hotmail.com>
2023-03-04 14:14:19 +0100

    Rest-4

--- Exercise.js
+++ Exercise.js
@@ -1,7 +1,21 @@
-function sum(numbers) {
-  let [num1, num2, num3] = [...numbers]; 
+//Soluzione con spread operator ma non è possibile andarlo a ciclare o al meno sarebbe inutile andarlo a ciclare utilizzando uno spread operator
+function sum(num1, num2, num3) {
   return num1 + num2 + num3;
 }
 
 const numbers = [1, 2, 3];
-console.log(sum(numbers));
+console.log(sum(...numbers));
+
+
+
+//Soluzione senza spread operator ma con possibilità di ingrandire l'array 
+function sum2(numbers) {
+  let sum = 0;
+  numbers.forEach(element => {
+    sum = element + sum;
+  });
+  return sum;
+}
+
+const numbers2 = [1, 2, 3, 5, 6, 7, 8, 9];
+console.log(sum2(numbers2));
