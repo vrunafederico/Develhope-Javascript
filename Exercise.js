f95971fc1c6a2478487dbd9b435343f6f8f00dc5
vrunafederico <Federix5@hotmail.com>
2023-03-12 18:48:44 +0100

    Network_Requests-1

--- Exercise.js
+++ Exercise.js
@@ -1,10 +1,12 @@
-function sum(numbers) {
-  let sum = 0;
-  console.log(numbers)
-  numbers.forEach(element => console.log(element));
-  console.log(sum)
-  return sum;
+async function Responce(){
+  let val = await fetch("https://jsonplaceholder.typicode.com/todos");
+  let json = await val.json();
+  json.forEach(element => {
+      let ul = document.querySelector("ul");
+      let li = document.createElement("li")
+      li.innerText = element.title;
+      ul.appendChild(li);
+  });
 }
 
-const numbers = [1, 2, 3];
-console.log(sum(numbers));
+Responce();
