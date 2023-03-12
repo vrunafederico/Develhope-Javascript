b85bdad89a5804332693aaaff1be910926882d75
vrunafederico <Federix5@hotmail.com>
2023-03-12 18:04:48 +0100

    Promise Chaining

--- Exercise.js
+++ Exercise.js
@@ -1,10 +1,28 @@
-function sum(numbers) {
-  let sum = 0;
-  console.log(numbers)
-  numbers.forEach(element => console.log(element));
-  console.log(sum)
-  return sum;
-}
 
-const numbers = [1, 2, 3];
-console.log(sum(numbers));
+let isLogged = true; 
+let Control = (isLogged) => {return new Promise((resolve, reject) => {
+    isLogged ? resolve(Math.random()) : reject("false");
+  })};
+
+let Control2 = (Number) => {return new Promise((resolve, reject) => {
+    Number>0.5 ? resolve({nome: "John", età: 24}) : reject("minore di 0.5");
+  })};
+
+Control(isLogged)
+    .then((val) => Control2(val)
+        .then((val) => console.log(val))
+        .catch((err) => console.log(err)))
+    .catch((err) => console.log(err))
+
+
+async function Check(isLogged){
+    try{ 
+        let val = await Control(isLogged);
+        let check = await Control2(val);
+        console.log(check);
+    }catch(err){
+        console.log(err)
+    }
+} 
+   
+Check(isLogged);
