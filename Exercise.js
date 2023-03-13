a89483c09f34f7a32bdc648dd49bc168ca963cbc
vrunafederico <Federix5@hotmail.com>
2023-03-13 07:50:13 +0100

    Network_Requests-2

--- Exercise.js
+++ Exercise.js
@@ -1,10 +1,25 @@
-function sum(numbers) {
-  let sum = 0;
-  console.log(numbers)
-  numbers.forEach(element => console.log(element));
-  console.log(sum)
-  return sum;
+async function takeValue(){
+  let id = 4;
+  let todo = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+  let json = await todo.json();
+  return json;
 }
 
-const numbers = [1, 2, 3];
-console.log(sum(numbers));
+
+async function checkbox(){
+  try{
+    let object = await takeValue();
+    let h2 = document.createElement("h2");
+    h2.innerHTML = object.title;
+    document.body.appendChild(h2);
+  
+    let checkbox = document.createElement("INPUT");
+    checkbox.setAttribute("type", "checkbox");
+    object.completed ? checkbox.checked = true : checkbox.checked = false; 
+    document.body.appendChild(checkbox);
+  }catch(err){
+    console.err(err);
+  }
+}
+
+checkbox();
